7151f868f3530c045d37843c65b5813cb93ef219
martomz <tomataomarjohn@gmail.com>
2023-09-16 19:46:58 +0800

    Initial scss variables

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,16 @@
 const Home = () => {
     return ( 
-        <>Hello</>
+        <div className="main-container">
+            <div>
+                <h1>Web App Features and Components</h1>
+                <p>
+                    The following are sample features and components that I can further develop and implement on any website.
+                </p>
+                <p>
+                    The components I created basically follows the mobile-first design principle and are built using the MERN stack together with Vite, TypeScript, and SASS in the development process.
+                </p>
+            </div>
+        </div>
     );
 }
  
