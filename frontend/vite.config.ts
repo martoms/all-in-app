5ed1e8c8223139a341bde2d3bfbaed5cd05ba389
martomz <tomataomarjohn@gmail.com>
2023-10-02 15:40:46 +0800

    revert to http

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-// import https from 'https';
-import fs from 'fs';
-import * as path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.join('C:/Windows/System32/localhost-key.pem')),
-      cert: fs.readFileSync(path.join('C:/Windows/System32/localhost.pem')),
-    },
-  },
-});
+})
